4db423ae9cb5ee4d459fb4348257a16df6cf1958
aitest-lang <aitestall@outlook.com>
2025-03-11 19:58:46 +0530

    Update build.sh

--- citeNet/build.sh
+++ citeNet/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-set -o errexit
+set -o errexit  # Exit immediately if a command exits with a non-zero status
+
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Apply database migrations
 python manage.py migrate
 
 # Create a superuser if it doesn't exist
